4ea3479de178497f1e2f1e0293eb42dc5a3f6975
Hugo <hugodovs@gmail.com>
2017-06-08 19:52:35 -0300

    main added

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+
+//Directory:
+//Convert file_name into directory_entries
+
+
+#define DISK_CAPACITY 20
+int secondary_mem[DISK_CAPACITY];
+write_function
+read_function
+
+
+
+
+int primary_mem[5];
+
+
+int main (void){
+
+    //Define a file:
+    
+
+    //Print state function
+    
+    
+
+
+    return 0;
+}
