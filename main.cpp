53d0ae2727ea174b90d69f55ecae28c303f7cc37
Hugo <hugodovs@gmail.com>
2017-06-17 18:51:57 -0300

    screen updated

--- main.cpp
+++ main.cpp
@@ -8,84 +8,24 @@
 #include "headers/ram_file_table.h"
 #include "headers/workflow.h"
 
-//teste
-// Files
-char file1[] = "hugo";
-//char file2[] = "joao_burro";
-
+#define clear() printf("\033[H\033[J")
+#define gotoxy(x,y) printf("\033[%d;%dH", (x), (y))
 
 int main (void){
 	// Init Enviroment
-
-    //init_screen();
+    init_screen();
     init_disk();
     init_ram();
-    /*update_screen();
 
-    if (write_onDisk(file1, "file1.txt") == -1) puts("MErda no disco");*/
+    //Main loop:
+    while(1) {
 
-    //update_file_table_id_buffer();
-
-    /*update_log_buffer("Schulz eh um viadinho");
-    update_screen();
-
-    update_log_buffer("Joao tambem eh");
-    update_screen();
-
-    update_log_buffer("Eles foram dormir");
-    update_screen();
-
-    update_log_buffer("E me deixaram aqui");
-    update_screen();
-
-    update_log_buffer("Fazendo essa merda");
-    update_screen();
-
-    update_log_buffer("DE TRABALHO DE MERDA iuaheuiahea");
-    update_screen();*/
-    //update_log_buffer("asdasd");
-    //update_screen();
-    //write_onDisk(file1, "file1.txt");
-	//open_file("file1.txt");
-    //print_ComputerStatus();
-
-    //Print file
-   	//printf("%s\n", file1);
-   	//write_onDisk(file1, "file1.txt");  // precisa de if aqui... com o int retornado
-   	//read_fromDisk("file1.txt", fileOut);
-    //delete_fromDisk("file1.txt");
-    //write_onDisk(file2, "file2.txt");
-    //print_ComputerStatus();
-    //print_ComputerStatus();
-    //open_file("file1.txt");
-    //print_ComputerStatus();
-    //clearScreen();
-    //print_ComputerStatus();
-    //char fileOut[100];
-    //printf("\nFileOut: %s\n", fileOut);
-    //read_fromDisk("file2.txt", fileOut);
-	//printf("\nFileOut: %s\n", fileOut);
-    //get_next_Instruction();
-    /*open_file("file1.txt", "w");
-    open_file("file2.txt", "r");
-    write_on_file("file2.txt", "namoralvsf");
-    printf("size : %d\n", files_ram_table[1].file_size);
-    read_from_file("file2.txt", 3);
-    close_file("file2.txt");
-
-    delete_file("file2.txt");
-    print_inode_table();*/
-    //write_on_file("file1.txt", "joao");
-    //write_on_file("file1.txt", "mateus");
-    //close_file("file1.txt");
-    while(1) {get_next_Instruction();
-      print_disk();
-      print_ram();
+        update_screen();
+        gotoxy(39, 15);
+        get_next_Instruction();
+        update_screen();
       
     }
-    //update_screen();
-    //while(1);
-    // Print (after Copy)
-    //print_ComputerStatus();
+
     return 0;
 }
--- src/screen.cpp
+++ src/screen.cpp
@@ -12,29 +12,29 @@
 #define ROW_NUMBER 40
 #define COL_NUMBER 120
 
-char screen_buffer[4801] = "+======================================================================================================================+"
-							"|         COMPUTER SPECS                                                                                               |"
+char screen_buffer[4801] =  "+======================================================================================================================+"
 							"|                                                                                                                      |"
 							"|                                                                                                                      |"
 							"|                                                                                                                      |"
 							"|                                                                                                                      |"
 							"|                                                                                                                      |"
-							"+======================================================================================================================+"
-							"|                                         |                        |                                                   |"
-							"|             FILE TABLE                  |       RAM MEMORY       |             HARD DRIVE MEMORY                     |"
-							"|                                         |                        |                                                   |"
-							"|  ID          ADDRESS         PATH       | 0: [   ]     10: [   ] |    0: [   ]  10: [   ]  20: [   ]  30 [   ]       |"
-							"| +===+        +====+      +============+ | 1: [   ]     11: [   ] |    1: [   ]  11: [   ]  21: [   ]  31 [   ]       |"
-							"| |   |        |    |      |            | | 2: [   ]     12: [   ] |    2: [   ]  12: [   ]  22: [   ]  32 [   ]       |"
-							"| |===+        +====+      +============+ | 3: [   ]     13: [   ] |    3: [   ]  13: [   ]  23: [   ]  33 [   ]       |"
-							"| |   |        |    |      |            | | 4: [   ]     14: [   ] |    4: [   ]  14: [   ]  24: [   ]  34 [   ]       |"
-							"| |===+        +====+      +============+ | 5: [   ]     15: [   ] |    5: [   ]  15: [   ]  25: [   ]  35 [   ]       |"
-							"| |   |        |    |      |            | | 6: [   ]     16: [   ] |    6: [   ]  16: [   ]  26: [   ]  36 [   ]       |"
-							"| |===+        +====+      +============+ | 7: [   ]     17: [   ] |    7: [   ]  17: [   ]  27: [   ]  37 [   ]       |"
-							"| |   |        |    |      |            | | 8: [   ]     18: [   ] |    8: [   ]  18: [   ]  28: [   ]  38 [   ]       |"
-							"| +===+        +====+      +============+ | 9: [   ]     19: [   ] |    9: [   ]  19: [   ]  29: [   ]  39 [   ]       |"
-							"| |   |        |    |      |            | |                        |                                                   |"
-							"| +===+        +====+      +============+ +========================+===================================================+"
+							"|                                                                                                                      |"
+							"+=========================================+=========================+==================================================+"
+							"|                                         |                         |                                                  |"
+							"|           FILE RAM TABLE                |       RAM MEMORY        |            HARD DRIVE MEMORY                     |"
+							"|                                         |                         |                                                  |"
+							"|  SIZE        ADDRESS         PATH       | 0: > [   ]  10: > [   ] |   0: [   ]  10: [   ]  20: [   ]  30 [   ]       |"
+							"| +===+        +====+      +============+ | 1:   [   ]  11:   [   ] |   1: [   ]  11: [   ]  21: [   ]  31 [   ]       |"
+							"| |   |        |    |      |            | | 2:   [   ]  12:   [   ] |   2: [   ]  12: [   ]  22: [   ]  32 [   ]       |"
+							"| +===+        +====+      +============+ | 3:   [   ]  13:   [   ] |   3: [   ]  13: [   ]  23: [   ]  33 [   ]       |"
+							"| |   |        |    |      |            | | 4:   [   ]  14: > [   ] |   4: [   ]  14: [   ]  24: [   ]  34 [   ]       |"
+							"| +===+        +====+      +============+ | 5:   [   ]  15:   [   ] |   5: [   ]  15: [   ]  25: [   ]  35 [   ]       |"
+							"| |   |        |    |      |            | | 6:   [   ]  16:   [   ] |   6: [   ]  16: [   ]  26: [   ]  36 [   ]       |"
+							"| +===+        +====+      +============+ | 7:   [   ]  17:   [   ] |   7: [   ]  17: [   ]  27: [   ]  37 [   ]       |"
+							"| |   |        |    |      |            | | 8:   [   ]  18:   [   ] |   8: [   ]  18: [   ]  28: [   ]  38 [   ]       |"
+							"| +===+        +====+      +============+ | 9:   [   ]  19: > [   ] |   9: [   ]  19: [   ]  29: [   ]  39 [   ]       |"
+							"| |   |        |    |      |            | |                         |                                                  |"
+							"| +===+        +====+      +============+ +=========================+==================================================+"
 							"| |   |        |    |      |            | |                                                                            |"
 							"| +===+        +====+      +============+ |                                   LOG                                      |"
 							"| |   |        |    |      |            | | +========================================================================+ |"
@@ -54,46 +54,46 @@ char screen_buffer[4801] = "+===================================================
 							"+======================================================================================================================+";
 
 //Buffers:
-char file_table_id_buffer[9];
+char file_table_size_buffer[10];
 char file_table_address_buffer[20];
 char file_table_path_buffer[100];
 char ram_memory_buffer[20];
 char hard_drive_buffer[40];
-char log_buffer[528]; //66 per line
+char log_buffer[520]; //65 per line
 
-void restart_file_table_id_buffer(){
-	for(int i = 0; i < 9; i++){
-		file_table_id_buffer[i] = '_';
+void restart_file_table_size_buffer(){
+	for(int i = 0; i < 10; i++){
+		file_table_size_buffer[i] = ' ';
 	}
 }
 
 void restart_file_table_address_buffer(){
 	for(int i = 0; i < 20; i++){
-		file_table_id_buffer[i] = '_';
+		file_table_address_buffer[i] = ' ';
 	}
 }
 
 void restart_file_table_path_buffer(){
 	for(int i = 0; i < 100; i++){
-		file_table_id_buffer[i] = '.';
+		file_table_path_buffer[i] = ' ';
 	}
 }
 
 void restart_ram_memory_buffer(){
 	for(int i = 0; i < 20; i++){
-		file_table_id_buffer[i] = '_';
+		ram_memory_buffer[i] = ' ';
 	}
 }
 
 void restart_hard_drive_buffer(){
 	for(int i = 0; i < 40; i++){
-		file_table_id_buffer[i] = '_';
+		hard_drive_buffer[i] = ' ';
 	}
 }
 
 void restart_log_buffer(){
-	for(int i = 0; i < 528; i++){
-		file_table_id_buffer[i] = '_';
+	for(int i = 0; i < 520; i++){
+		log_buffer[i] = ' ';
 	}
 }
 
@@ -105,7 +105,7 @@ void init_screen()
 	    std::system("resize -s 40 120 >/dev/null");
 	#endif
 
-	restart_file_table_id_buffer();
+	restart_file_table_size_buffer();
 	restart_file_table_address_buffer();
 	restart_file_table_path_buffer();
 	restart_ram_memory_buffer();
@@ -126,49 +126,125 @@ void update_screen(){
 	sleep(1);
 }
 
-void update_file_table_id_buffer(){
-	for(int i = 0; i < 1; i++){
-		file_table_id_buffer[i] = directory_table[i].id_file;
+void update_file_table_size_buffer(){
+	for(int i = 0; i < 10; i++){
+		file_table_size_buffer[i] = 's';//files_ram_table[i].file_size;
 	}
 
+	//Atualiza screen buffer
 	int j = 0;
 	for(int i = 1685-120; i < 1685 + 120*1; i+=120){
-		screen_buffer[i] = file_table_id_buffer[j];
+		screen_buffer[i] = file_table_size_buffer[j];
 		j++;
 	}
 }
 
 void update_file_table_address_buffer(){
 	for(int i = 0; i < MAX_FILE_NUMBER; i++){
-		file_table_id_buffer[i] = inode_table[i].address_file;
+		file_table_size_buffer[i] = inode_table[i].address_file;
 	}
+
+	//Atualiza screen buffer
 }
 
 void update_file_table_path_buffer(){
 	for(int i = 0; i < MAX_FILE_NUMBER; i++){
 		for(int j = 0; j < 10; j++){
-			file_table_id_buffer[i] = *(directory_table[i].filename)+j;
+			file_table_size_buffer[i] = *(directory_table[i].filename)+j;
 		}
-
 	}
+
+	//Atualiza screen buffer	
 }
 
 void update_ram_memory_buffer(){
 	for (int i = 0; i <  RAM_CAPACITY; i++) {
 		ram_memory_buffer[i] = primary_mem[i];
 	}
+
+	//Atualiza screen buffer
 }
 
 void update_hard_drive_buffer(){
 	for (int i = 0; i <  DISK_CAPACITY; i++) {
 		hard_drive_buffer[i] = secondary_mem[i];
 	}
+	
+	//Atualiza screen buffer
 }
 
-void update_log_buffer(){
-	for(int i = 0; i < 528; i++){
-		file_table_id_buffer[i] = '_';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void update_log_buffer(char* new_line){
+	
+	for(int i = 0; i < 65; i++){
+		log_buffer[i] = log_buffer[i+65];
 	}
+
+	for(int i = 65; i < 65*2; i++){
+		log_buffer[i] = log_buffer[i+65];
+	}
+
+	for(int i = 65*2; i < 65*3; i++){
+		log_buffer[i] = log_buffer[i+65];
+	}
+
+	for(int i = 65*3; i < 65*4; i++){
+		log_buffer[i] = log_buffer[i+65];
+	}
+
+	for(int i = 65*4; i < 65*5; i++){
+		log_buffer[i] = log_buffer[i+65];
+	}
+
+	for(int i = 65*5; i < 65*6; i++){
+		log_buffer[i] = log_buffer[i+65];
+	}
+
+	for(int i = 65*6; i < 65*7; i++){
+		log_buffer[i] = log_buffer[i+65];
+	}
+
+	//New string:
+	//Reset:
+	for(int i = 65*7; i < 65*8; i++){
+		log_buffer[i] = '-';
+	}
+
+	int j = 0;
+	for(unsigned int i = 65*7; i < 65*7 + strlen(new_line); i++){
+		log_buffer[i] = *(new_line + j);
+		j++;
+	}
+
+	//Atualiza screen buffer
+	int k = 3171;
+	for (int j = 0; j < 7; j++){
+		for(int i = 0; i < 65; i++){
+			screen_buffer[k + i] = log_buffer[i + 65*j];
+		}
+		k += 120;
+	}
+
+	k+=120;
+	for(int i = 0; i < 65; i++){
+		screen_buffer[k + i] = log_buffer[i + 65*7];
+	}
+
+	
 }
 
 
