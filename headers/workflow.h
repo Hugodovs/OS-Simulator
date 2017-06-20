4666b6f314e14b3e8e926e466d4ec4e516bc1fba
jvitordeoliveira96 <jvitordeoliveira@outlook.com>
2017-06-19 22:20:59 -0300

    Add files via upload

--- headers/workflow.h
+++ headers/workflow.h
@@ -7,7 +7,8 @@ extern char primary_mem[RAM_CAPACITY];
 
 void get_next_Instruction();
 void execute_open_Operation(char *file);
-void execute_read_Operation(char *file);
-void execute_write_Operation(char *file);
+void execute_read_Operation(char *file, int size);
+void execute_write_Operation(char *file, char *newdata);
 void execute_close_Operation(char *file);
+void execute_delete_Operation (char *fileName);
 char* strseparator(char** stringp, const char* delim);
